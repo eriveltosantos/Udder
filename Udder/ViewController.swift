494077881830f82ee1620e5f1c5b4a1213bd15e2
Erivelto dos Santos <eriveltosantos@yahoo.com.br>
2020-06-05 17:50:13 -0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Erivelto Santos on 05/06/20.
+//  Copyright © 2020 Erivelto Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
